af3d87133ad8a17c0ca387b023bcf9a7132800dd
Santosh Dahit <santoshdahit455@gmail.com>
2024-03-05 00:14:11 +0900

    task 2.1.2

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,21 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+
+    @Bean (name = "cat")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("from pojo  class cat");
+        return cat;
+    }
+}
